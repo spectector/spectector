084dd098b31d695a1177ad831f45f85005afd31a
Jose F. Morales <jfmcjf@gmail.com>
2018-12-16 07:46:27 +0100

    Added simple command to generate binary distributions
    Usage:
    
     $ ciao custom_run . bindist
    
    This calls `make_distro.sh` script with some environment variables
    needed to locate the right binaries. The binary distribution is stored
    at `dist/` under the `spectector/` directory. Additionally a .tar.gz
    file with the distribution is generated.

--- Manifest/make_distro.sh
+++ Manifest/make_distro.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
-mkdir -p spectector/bin
-mkdir -p spectector/benchmarks
-mkdir -p spectector/results/out
-mkdir -p spectector/results/time
-cd code
-ciaoc -S spectector.pl || (echo "Failed at compilation" && exit 1) # Makes standalone executable
-cd - > /dev/null
-# With ln we don't replicate info
-ln -f code/spectector spectector/bin/spectector
-ln -f code/third-party/bin/z3 spectector/bin/z3
-cp README.md spectector/README.md
-cp -r benchmarks/*.sh spectector/benchmarks
-cp -r benchmarks/clang spectector/benchmarks
-cp -r benchmarks/intel spectector/benchmarks
-cp -r benchmarks/microsoft spectector/benchmarks
-cp -r benchmarks/source spectector/benchmarks
-cp -r benchmarks/tests spectector/benchmarks
-cp -r benchmarks/*py spectector/benchmarks
+# Binary distribution for artifact evaluation
+# (contains benchmarks) 
+#
+# Usage: 
+#   $ ciao custom_run . bindist
+#
+# (NOTE: Do NOT CALL this script directly)
 
+# Physical directory where the script is located
+_base=$(e=$0;while test -L "$e";do d=$(dirname "$e");e=$(readlink "$e");\
+    cd "$d";done;cd "$(dirname "$e")";pwd -P)
+
+# (Assume that we have cloned it there)
+benchmarks="$_base"/../../spectector-benchmarks
+distdir="$_base"/../dist
+
+# Create directory for distribution
+mkdir -p "$distdir"
+
+# Prepare binaries
+mkdir -p "$distdir"/spectector/bin
+cd "$distdir"/spectector/bin
+ciaoc -S -o spectector "$_base"/../src/spectector.pl || (echo "Failed at compilation" && exit 1)
+# Copy Z3 (THIRDPARTY is defined externally)
+cp "$THIRDPARTY"/bin/z3 .
+
+# Copy READMEs
+cd "$distdir"/spectector
+cp "$_base"/../README.md README.md
+
+# Copy benchmarks
+mkdir -p "$distdir"/spectector/benchmarks
+cd "$distdir"/spectector/benchmarks
+cp "$benchmarks"/check_security.sh .
+cp -r "$benchmarks"/target .
+cp -r "$benchmarks"/sources .
+
+cd "$distdir"
 tar cfvz spectector.tar.gz spectector
-mv spectector dist
+
---
+++ Manifest/spectector.hooks.pl
@@ -0,0 +1,19 @@
+:- module(_, [], [ciaobld(bundlehooks)]).
+
+:- doc(title,  "Bundle Hooks for spectector").
+
+:- use_module(engine(internals), [ciao_root/1]).
+:- use_module(library(streams)).
+:- use_module(library(bundle/bundle_paths)).
+:- use_module(library(pathnames)).
+:- use_module(library(sh_process), [sh_process_call/3]).
+
+% (hook)
+'$builder_hook'(custom_run(bindist, [])) :- !,
+	% Create a binary distribution
+	Script = ~bundle_path(spectector, 'Manifest/make_distro.sh'),
+	ThirdParty = ~path_concat(~ciao_root, 'third-party'),
+	Env = ['THIRDPARTY' = ThirdParty],
+	sh_process_call(Script, [], [env(Env)]).
+
+	
