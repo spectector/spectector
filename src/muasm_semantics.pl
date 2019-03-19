0c1d1699ecb0af79ea05f84e88b91cea92e5a162
Andrés Sánchez <andres.sanchez@imdea.org>
2019-03-19 10:41:14 +0100

    Typo on semantic rule. When unknown instruction is reached, stops

--- src/muasm_semantics.pl
+++ src/muasm_semantics.pl
@@ -139,8 +139,9 @@ run_(skip,c(M,A)) := c(M,A2) :-
 run_(unknown_ins(I),c(M,A)) := c(M,A2) :-
 	message(warning, ['Pass through an unsupported instruction! ', I]),
 	increment_unknown_instructions,
-	A2 = ~update0(A,pc,~incpc(A)).
-run_(unknwown_pc(L),c(M,A)) := c(M,A2) :-
+	A2 = ~update0(A,pc,-1). % TODO: Standard site to jump?
+%A2 = ~update0(A,pc,~incpc(A)).
+run_(unknown_pc(L),c(M,A)) := c(M,A2) :-
 	message(warning, ['Pass through a non declared Label! ', L]),
 	new_unknown_label(string(~atom_codes(L))),
 	A2 = ~update0(A,pc,-1). % TODO: Standard site to jump?
